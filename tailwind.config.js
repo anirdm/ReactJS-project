16eb59cf84ad87bda02e2e07f74d90c1c1503d45
Dimitrina Nestorova <136362960+anirdm@users.noreply.github.com>
2024-07-18 18:27:05 +0300

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+     extend: {
+      colors: {
+        'bright-white': '#f4f5f0',
+        'flagstone': '#aba9a9',
+        'blue-mana': '#5ec2f5',
+      }
+     },
   },
   plugins: [],
 }
